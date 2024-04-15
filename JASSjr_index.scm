1bda4c8e7a1a6d9b3601913709b47f23200af1c1
Vaughan Kitchen <contact@tophat.nz>
2024-04-16 08:43:37 +1200

    Word by word

--- JASSjr_index.scm
+++ JASSjr_index.scm
@@ -1,13 +1,24 @@
 #!/usr/bin/env -S csi -r5rs-syntax -ss
 
-(import scheme (chicken io) (chicken process-context))
+(import
+  scheme
+  (chicken io)
+  (chicken irregex)
+  (chicken process-context))
+
+(define (print-list lst)
+  (if (null? lst)
+    '()
+    (begin
+      (print (car lst))
+      (print-list (cdr lst)))))
 
 (define (index file)
   (let loop ((line (read-line file)))
     (if (eof-object? line)
       (close-input-port file)
       (begin
-	(print line)
+	(print-list (irregex-extract "[a-zA-Z0-9][a-zA-Z0-9-]*|<[^>]*>" line))
 	(loop (read-line file))))))
 
 (define (main args)
