107f8ed9146cc8a59b8db1f9622fa5bf4f9b3259
Vaughan Kitchen <contact@tophat.nz>
2024-04-20 19:33:05 +1200

    Regex based parser

--- JASSjr_index.exs
+++ JASSjr_index.exs
@@ -6,115 +6,89 @@
 # Minimalistic BM25 search engine.
 
 defmodule Index do
-  defstruct length: 0, # length of currently indexing document
-  docno: 0, # cache last index into primary keys
+  defstruct doclength: 0, # length of currently indexing document
+  docid: -1, # cache last index into primary keys
   doclengths: [], # hold the length of each document
-  docnos: [], # the primary keys
-  terms: %{} # the in-memory index (terms => <tf, docid>)
+  docids: [], # the primary keys
+  vocab: %{}, # the in-memory index (terms => <tf, docid>)
+  pushnext: false # is the next token the primary key?
 
   # Add the posting to the in-memory index
   def append(index, term) do
-      docid = index.docno - 1
-      %Index{index | length: index.length + 1, terms: Map.update(index.terms, term, [ 1, docid ], fn [ tf | [ doc | tail ]] = docnos ->
-        if doc != docid do
-          # if the docno for this occurence has changed then create a new <d,tf> pair
-          [ 1 | [ docid | docnos ]]
-        else
-          # else increase the tf
-          [ tf + 1 | [ doc | tail ]]
-        end
-      end)
-    }
+    %Index{index | doclength: index.doclength + 1, vocab: Map.update(index.vocab, term, [ 1, index.docid ], fn [ tf | [ doc | tail ]] = docnos ->
+      if doc != index.docid do
+        # if the docno for this occurence has changed then create a new <d,tf> pair
+        [ 1 | [ index.docid | docnos ]]
+      else
+        # else increase the tf
+        [ tf + 1 | [ doc | tail ]]
+      end
+    end)}
   end
 end
 
 defmodule Indexer do
-  def consume_tag(<<head, tail::binary>>, index) do
-    case head do
-      # '>'
-      62 -> parse(tail, index)
-      _ -> consume_tag(tail, index)
-    end
-  end
-
-  def parse_tag(file, index) do
-    if String.starts_with?(file, "<DOCNO>") do
-      # If this is a <DOCNO> parse the primary key
-      if index.docno |> rem(1000) == 0 do
-        IO.puts("#{index.docno} documents indexed")
-      end
-
-      {_, file} = String.split_at(file, 7)
-      [docno, file] = String.split(file, "</DOCNO>", parts: 2)
-      docno = String.trim(docno)
-
-      # Move on to the next document
-      index = if index.docno > 0 do
-        %Index{index | length: 0, doclengths: [ index.length | index.doclengths], docno: index.docno + 1, docnos: [ docno | index.docnos]}
-      else
-        %Index{index | docno: index.docno + 1, docnos: [ docno | index.docnos]}
-      end
-
-      # Include the primary key as a term to match the other indexers
-      index = Index.append(index, docno)
-
-      parse(file, index)
-    else
-      # Otherwise consume until end of tag
-      consume_tag(file, index)
-    end
-  end
-
-  def parse_alnum(file, index, val \\ <<>>)
-  def parse_alnum(<<>>, index, val), do: Index.append(index, val)
-  def parse_alnum(<<head, tail::binary>> = file, index, val) do
-    case head do
-      # Numeric
-      x when x in 48..57 -> parse_alnum(tail, index, val <> <<x>>)
-      # Uppercase
-      x when x in 65..90 -> parse_alnum(tail, index, val <> <<x+32>>) # lower case the string
-      # Lowercase
-      x when x in 97..122 -> parse_alnum(tail, index, val <> <<x>>)
-      # Hyphen is allowed after the initial character
-      45 -> parse_alnum(tail, index, val <> <<head>>)
-      _ -> parse(file, Index.append(index, val))
-    end
-  end
-
   # One-character lookahead lexical analyser
-  def parse(file, index \\ %Index{})
   def parse(<<>>, index), do: index
-  def parse(<<head, tail::binary>> = file, index) do
+  def parse(line, index) do
     # A token is either an XML tag '<'..'>' or a sequence of alpha-numerics.
-    case head do
-      # Tag '<'
-      60 -> parse_tag(file, index)
-      # Numeric
-      x when x in 48..57 -> parse_alnum(file, index)
-      # Uppercase
-      x when x in 65..90 -> parse_alnum(file, index)
-      # Lowercase
-      x when x in 97..122 -> parse_alnum(file, index)
-      # Skip over whitespace and punctuation
-      _ -> parse(tail, index)
-    end
+    # TREC <DOCNO> primary keys have a hyphen in them
+    Regex.scan(~r/[a-zA-Z0-9][a-zA-Z0-9-]*|<[^>]*>/, line)
+    |> Enum.reduce(index, fn [ token | _ ], acc ->
+      # If we see a <DOC> tag then we're at the start of the next document
+      cond do
+        token == "<DOC>" ->
+          # Save the previous document length
+          acc = if acc.docid != -1 do
+            %Index{acc | doclengths: [ acc.doclength | acc.doclengths]}
+          else
+            acc
+          end
+
+          # Move on to the next document
+          acc = %Index{acc | docid: acc.docid + 1, doclength: 0}
+
+          if acc.docid |> rem(1000) == 0 do
+            IO.puts("#{acc.docid} documents indexed")
+          end
+
+          acc
+        # if the last token we saw was a <DOCNO> then the next token is the primary key
+        acc.pushnext ->
+          # Include the primary key as a term to match the other indexers
+          acc = %Index{acc | docids: [ token | acc.docids]}
+          acc = Index.append(acc, token)
+          %Index{acc | pushnext: false }
+        token == "<DOCNO>" ->
+          %Index{acc | pushnext: true }
+        # Don't index XML tags
+        String.first(token) == "<" ->
+          acc
+        true ->
+           Index.append(acc, String.downcase(token)) # lower case the string
+      end
+    end)
   end
 
   # serialise the in-memory index to disk
   def serialise(index) do
     # Save the final document length
-    index = %Index{index | doclengths: [ index.length | index.doclengths]}
-    docnos = Enum.reverse(index.docnos)
+    index = %Index{index | doclengths: [ index.doclength | index.doclengths]}
+
+    # Tell the user we've got to the end of parsing
+    IO.puts("Indexed #{index.docid} documents. Serialising...")
+
+    docids = Enum.reverse(index.docids)
 
     # store the primary keys
     File.open!("docids.bin", [:write], fn file ->
-      Enum.each(docnos, fn docno ->
-        IO.write(file, "#{docno}\n")
+      Enum.each(docids, fn docid ->
+        IO.write(file, "#{docid}\n")
       end)
     end)
     vocab = File.open!("vocab.bin", [:write])
     postings = File.open!("postings.bin", [:write])
-    Enum.each(index.terms, fn {term, posts} ->
+    Enum.each(index.vocab, fn {term, posts} ->
       # write the postings list to one file
       posts = Enum.reverse(posts)
       posts = for x <- posts, do: <<x::native-32>>, into: <<>>
@@ -134,18 +108,20 @@ defmodule Indexer do
       IO.binwrite(file, doclengths)
     end)
   end
+
+  def start() do
+    # Make sure we have one parameter, the filename
+    if length(System.argv()) != 1 do
+      IO.puts("Usage: ./JASSjr_index.exs <infile.xml>")
+      System.halt()
+    end
+
+    # Read the file to index
+    [ filename | _ ] = System.argv()
+    File.stream!(filename)
+    |> Enum.reduce(%Index{}, &parse/2)
+    |> serialise
+  end
 end
 
-# Make sure we have one paramter, the filename
-if length(System.argv()) != 1 do
-  IO.puts("Usage: ./JASSjr_index.exs <infile.xml>")
-  System.halt()
-end
-
-# Read the file to index
-[ filename | _ ] = System.argv()
-file = File.read!(filename)
-
-index = Indexer.parse(file)
-IO.puts("Indexed #{index.docno} documents. Serialising...") # tell the user we've got to the end of parsing
-Indexer.serialise(index)
+Indexer.start()
