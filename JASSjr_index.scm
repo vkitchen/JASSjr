b1ab7fe30bba2b1d864d51f0185659233286fed8
Vaughan Kitchen <contact@tophat.nz>
2026-09-25 18:57:45 +1200

    Fix doc-lengths

--- JASSjr_index.scm
+++ JASSjr_index.scm
@@ -31,62 +31,60 @@
     (map char-downcase (string->list s))))
 
 (define (index tokens)
-  (if (null? tokens)
-    '()
+  (unless (null? tokens)
     (let ((token (car tokens)))
-      (begin
-        ; If we see a <DOC> tag then we're at the start of the next document
-        (if (string=? token "<DOC>")
-          (begin
-            ; Save the previous document length
-            (if (not (= docid -1))
-              (set! doc-lengths (cons document-length doc-lengths)))
+      ; If we see a <DOC> tag then we're at the start of the next document
+      (if (string=? token "<DOC>")
+        (begin
+          ; Save the previous document length
+          (if (not (= docid -1))
+            (set! doc-lengths (cons document-length doc-lengths)))
 
-            ; Move on to the next document
-            (set! docid (+ docid 1))
-            (set! document-length 0)
+          ; Move on to the next document
+          (set! docid (+ docid 1))
+          (set! document-length 0)
 
-            (if (= (modulo docid 1000) 0)
-              (print docid " documents indexed"))))
+          (if (= (modulo docid 1000) 0)
+            (print docid " documents indexed"))))
 
-          ; if the last token we saw was a <DOCNO> then the next token is the primary key
-        (if push-next
-          (begin
-            (set! doc-ids (cons token doc-ids))
-            (set! push-next #f)))
-        (if (string=? token "<DOCNO>") (set! push-next #t))
+      ; if the last token we saw was a <DOCNO> then the next token is the primary key
+      (if push-next
+        (begin
+          (set! doc-ids (cons token doc-ids))
+          (set! push-next #f)))
+      (if (string=? token "<DOCNO>") (set! push-next #t))
 
-        ; Don't index XML tags
-        (if (not (char=? (string-ref token 0) #\<))
-          (let* (
-            ; lower case the string
-            (lowercase (string-downcase token))
+      ; Don't index XML tags
+      (if (not (char=? (string-ref token 0) #\<))
+        (let* (
+          ; lower case the string
+          (lowercase (string-downcase token))
 
-            ; truncate any long tokens at 255 charactes (so that the length can be stored first and in a single byte)
-            (lowercase (substring lowercase 0 (min 255 (string-length lowercase)))))
+          ; truncate any long tokens at 255 charactes (so that the length can be stored first and in a single byte)
+          (lowercase (substring lowercase 0 (min 255 (string-length lowercase)))))
 
-            ; add the posting to the in-memory index
-            (let ((postings (hash-table-ref/default vocab lowercase #f)))
-              (cond
-                ; if the term isn't in the vocab yet
-                ((not postings)
-                 (hash-table-set! vocab lowercase
-                    (list docid 1)))
+          ; add the posting to the in-memory index
+          (let ((postings (hash-table-ref/default vocab lowercase #f)))
+            (cond
+              ; if the term isn't in the vocab yet
+              ((not postings)
+               (hash-table-set! vocab lowercase
+                  (list docid 1)))
 
-                ; if the docno for this occurence has changed then create a new <d,tf> pair
-                ((not (= (car postings) docid))
-                  (hash-table-set! vocab lowercase
-                    (cons docid (cons 1 postings))))
+              ; if the docno for this occurence has changed then create a new <d,tf> pair
+              ((not (= (car postings) docid))
+                (hash-table-set! vocab lowercase
+                  (cons docid (cons 1 postings))))
 
-                ; else increase the tf
-                (else
-                  (set-car! (cdr postings)
-                    (+ (cadr postings) 1)))))))
+              ; else increase the tf
+              (else
+                (set-car! (cdr postings)
+                  (+ (cadr postings) 1)))))
 
-        ; Compute the document length
-        (set! document-length (+ document-length 1))
+          ; Compute the document length
+          (set! document-length (+ document-length 1))))
 
-    (index (cdr tokens))))))
+      (index (cdr tokens)))))
 
 (define (tokenise file)
   (let loop ((line (read-line file)))
